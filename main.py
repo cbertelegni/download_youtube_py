c50cdff11e3c08a51493ff2e1420e6d49ca2f2f0
cbertelegni <cristianbertelegni@gmail.com>
2016-07-24 16:58:20 -0300

    Closes #1

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python
 
 from pytube import YouTube
+from pydub import AudioSegment
 from pprint import pprint
 import time
 import os, sys
+import glob
 
 """
     Set output folder name and source list for download from youtube
 """
 output_folder = "videos"
+audio_folder = "audio"
 video_urls = [
 "https://www.youtube.com/watch?v=taENjQXJbl8",
-"https://www.youtube.com/watch?v=ZvA0uO66qoc",
-"https://www.youtube.com/watch?v=qM_5YzToXRw"
+# "https://www.youtube.com/watch?v=ZvA0uO66qoc"
 ]
 
-
+EXTRACT_AUDIO = True
+extension_list = ('*.mp4', '*.flv')
 
 STAMP = str(time.time()).split(".")[0]
 BASE = os.path.dirname(os.path.abspath(__file__))
 output_path =  os.path.join(BASE, output_folder)
+output_audio_path =  os.path.join(output_path, audio_folder)
 
 if __name__ == "__main__":
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)
+
 
     for video_path in video_urls:  
         yt = YouTube(video_path)
@@ -44,3 +49,17 @@ if __name__ == "__main__":
 
         print("Sleeping...")
         time.sleep(2)
+
+
+    # extarct audios
+    if EXTRACT_AUDIO:
+        if not os.path.exists(output_audio_path):
+            os.makedirs(output_audio_path)
+
+        os.chdir(output_path)
+        for extension in extension_list:
+            for video in glob.glob(extension):
+                mp3_filename = os.path.splitext(os.path.basename(video))[0] + '.mp3'
+                mp3_path =  os.path.join(output_audio_path, mp3_filename)
+                AudioSegment.from_file(video).export(mp3_path, format='mp3')
+
